9dda618e56587e15a0b0a221884ebf0a7b24ae62
moonseunghun <mangokkaksae2@gmail.com>
2023-09-18 12:59:38 +0900

    Chore : title 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:fluttertil/screens/syncfusionTotal.dart';
+// ignore: unused_import
+import 'package:fluttertil/screens/syncfusion_flutter_calendar.dart';
+import 'package:fluttertil/screens/syncfusion_flutter_charts.dart';
+// ignore: unused_import
 import 'package:fluttertil/routes/my_routes.dart';
-import 'package:splashscreen/splashscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,34 +15,57 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final goRouter = GoRouter(
-      initialLocation: '/splash', // 초기 경로 설정
-      navigatorKey: GlobalKey<NavigatorState>(),
-      builder: (BuildContext context, GoRouterState state) {
-        return MaterialApp.router(
-          routerDelegate: state.routerDelegate,
-          routeInformationParser: state.routeInformationParser,
-        );
-      },
-    );
-
     return MaterialApp(
-      title: 'BottomNavBar Example',
+      title: 'BottomNavBar',
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: Scaffold(
-        body: SplashScreen(
-          seconds: 2,
-          navigateAfterSeconds: '/Login', // 시간이 지난 후 이동할 경로
-          image: Image.asset('assets/main_logo.png'), // 이미지 파일 경로
-          backgroundColor: Colors.white, // 배경색
-          loaderColor: Colors.grey,
-          photoSize: 100.0, // 이미지 크기
-          loadingText: Text("Loading"), // 로딩 텍스트
-        ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [MyAppHome(), CalendarApp(), CombinedApp()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Ourtimes'),
       ),
-      onGenerateRoute: goRouter.router.generator,
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'ChartApp',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_month),
+            label: 'CalendarApp',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'TotalApp',
+          ),
+        ],
+      ),
     );
   }
 }
